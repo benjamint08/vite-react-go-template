4f8a01417fd400cd0f41fafc067b056ca9f57011
Ben Thomas <benjamint08@gmail.com>
2026-02-05 14:34:59 +0000

    Add request time to really see if Go is faster

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import {clearTodos, addTodo, deleteTodo} from "./functions/todos.js";
 function App() {
     const [todos, setTodos] = useState(null)
     const [newTodo, setNewTodo] = useState('')
+    const [lastReqTime, setLastReqTime] = useState("")
+
+    function updateLastReqTime(reqTime, resTime) {
+        let timeTookInMs = resTime.getTime() - reqTime.getTime();
+        setLastReqTime(`${timeTookInMs} ms`)
+    }
 
     useEffect(() => {
         const fetchData = async () => {
+            const nowTime = new Date();
             const response = await fetch('/api/todos')
+            const endTime = new Date();
+            updateLastReqTime(nowTime, endTime)
             const data = await response.json()
             setTodos(data)
         }
@@ -20,7 +29,12 @@ function App() {
           <h1 className={"text-2xl font-bold text-center"}>React, Vite, Tailwind and Go Stack</h1>
           {todos && todos.length > 0 && (
               <div className={"flex mt-5 justify-center"}>
-                  <button className={"bg-red-500 text-white p-2"} onClick={(e) => clearTodos(setTodos)}>Clear Todos</button>
+                  <button className={"bg-red-500 text-white p-2"} onClick={(e) => {
+                      const nowTime = new Date();
+                      clearTodos(setTodos)
+                      const endTime = new Date();
+                      updateLastReqTime(nowTime, endTime)
+                  }}>Clear Todos</button>
               </div>
           )}
           <div className={"flex mt-5 justify-center flex-col w-[50%] mx-auto"}>
@@ -28,7 +42,12 @@ function App() {
                   <ul>
                       {todos.map((todo, index) => (
                         <li key={index} className={"border border-gray-300 p-2 ml-2 mt-2"}>{todo}
-                            <span className={"text-red-500 ml-2 float-right font-bold hover:cursor-pointer"} key={"del-" + index} onClick={() => deleteTodo(index, todos, setTodos)}>X</span>
+                            <span className={"text-red-500 ml-2 float-right font-bold hover:cursor-pointer"} key={"del-" + index} onClick={() => {
+                                const nowTime = new Date();
+                                deleteTodo(index, todos, setTodos)
+                                const endTime = new Date();
+                                updateLastReqTime(nowTime, endTime)
+                            }}>X</span>
                         </li>
                     ))}
                   </ul>
@@ -36,10 +55,18 @@ function App() {
               {todos && (
                   <>
                       <input type="text" placeholder="Add a todo" className={"border border-gray-300 p-2 ml-2 mt-2 text-black"} value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-                      <button className={"bg-blue-500 text-white p-2 ml-2 mt-2"} onClick={(e) => addTodo(todos, newTodo, setTodos, setNewTodo)}>Add</button>
+                      <button className={"bg-blue-500 text-white p-2 ml-2 mt-2"} onClick={(e) => {
+                          const nowTime = new Date();
+                          addTodo(todos, newTodo, setTodos, setNewTodo)
+                          const endTime = new Date();
+                          updateLastReqTime(nowTime, endTime)
+                      }}>Add</button>
                   </>
               )}
           </div>
+          <footer>
+                <p className={"text-center mt-10 text-gray-500"}>Last request time: {lastReqTime}</p>
+          </footer>
       </div>
     </>
   )
